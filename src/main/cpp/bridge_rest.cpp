30a31bbd44459fa537209e820a7869609041b392
Robert Burger <robert.burger@dlr.de>
2021-06-28 10:16:57 +0200

    ADD: rendering simple rest services working

--- src/main/cpp/bridge_rest.cpp
+++ src/main/cpp/bridge_rest.cpp
@@ -38,6 +38,22 @@ BRIDGE_DEF(bridge_rest, bridge::rest);
 using namespace bridge;
 using namespace httpserver;
 
+// Checks whether `str' starts with `start' ignoring case
+static bool starts_with(const std::string& str, const std::string& start) {
+    if (&start == &str) 
+        return true; // str and start are the same string
+
+    if (start.length() > str.length()) 
+        return false;
+
+    for (size_t i = 0; i < start.length(); ++i) {
+        if (start[i] != str[i]) 
+            return false;
+    }
+
+    return true;
+}
+
 rest::rest(const char*& bridgename, YAML::Node& node) :
     bridge_base(bridgename, "bridge_rest", node),
     ws(create_webserver(8080).no_regex_checking()), res(this)
@@ -99,9 +115,15 @@ const std::shared_ptr<http_response> rest::render(const http_request& req) {
     //uint8_t svc[1024];
     //req.set_data(&svc[0], signature.c_str());
     //uint64_t adr = (uint64_t)&svc[0];
+    
+    std::string content = req.get_content();
+    log(verbose, "got content \"%s\"\n", content.c_str());
+    auto content_node = YAML::Load(content);
 
     std::string name = req.get_path();
     auto& _svc = service_map[name];
+    
+    log(verbose, "rendering \"%s\"\n", name.c_str());
 
     // request arguments
     robotkernel::service_arglist_t service_request;
@@ -115,6 +137,45 @@ const std::shared_ptr<http_response> rest::render(const http_request& req) {
             for (const auto& kv : *it) {
                 string key   = kv.first.as<string>();
                 string value = kv.second.as<string>();
+
+                log(verbose, "request field: %s %s\n", key.c_str(), value.c_str());
+
+                if (content_node[value]) {
+                    log(verbose, "  field found in json message\n");
+                } else {
+                    log(warning, "  field NOT found in json message\n");
+                }
+                
+                if (key == "string") {
+                    if (content_node[value]) {
+                        service_request.push_back(content_node[value].as<string>());
+                    } else {
+                        service_request.push_back(string("init"));
+                    }
+                }
+
+#define push_back_type(type) \
+                if (key == #type) {                                 \
+                    if (content_node[value]) {                      \
+                        log(verbose, "  pushing to service_request, %d\n", (type)content_node[value].as<uint64_t>()); \
+                        service_request.push_back((type)content_node[value].as<uint64_t>());         \
+                    } else {                                        \
+                        service_request.push_back((type)0);         \
+                    }                                               \
+                }
+
+                push_back_type(uint64_t);
+                push_back_type(int64_t);
+                push_back_type(uint32_t);
+                push_back_type(int32_t);
+                push_back_type(uint16_t);
+                push_back_type(int16_t);
+                push_back_type(uint8_t);
+                push_back_type(int8_t);
+                push_back_type(float);
+                push_back_type(double);
+#undef push_back_type
+
 
 //                string ln_dt = service_datatype_to_ln(key);
 //
@@ -232,130 +293,53 @@ const std::shared_ptr<http_response> rest::render(const http_request& req) {
                 string key   = kv.first.as<string>();
                 string value = kv.second.as<string>();
 
-                #define push_back_type(type)                            \
+                if (starts_with(key, "vector")) {
+                    const size_t equals_idx = key.find_first_of('/');
+                    if (std::string::npos != equals_idx) {
+                        string vector = key.substr(0, equals_idx);
+                        string real_key = key.substr(equals_idx + 1);
+
+                        const std::vector<robotkernel::rk_type> elem = service_response[i++];
+#define push_back_type(type)                            \
+                        if (real_key == #type) {                            \
+                            for (unsigned i = 0; i < elem.size(); ++i) {    \
+                                answer[value].push_back((type)elem[i]);     \
+                        } }
+                        
+                        push_back_type(uint64_t);
+                        push_back_type(int64_t);
+                        push_back_type(uint32_t);
+                        push_back_type(int32_t);
+                        push_back_type(uint16_t);
+                        push_back_type(int16_t);
+                        push_back_type(uint8_t);
+                        push_back_type(int8_t);
+                        push_back_type(float);
+                        push_back_type(double);
+//                        push_back_type(string);
+#undef push_back_type
+
+                    }
+                } else {
+#define push_back_type(type)                            \
                     if (key == #type) {                                 \
                         const type& v = service_response[i++];          \
                         answer[value] = v;                              \
                     }
- 
-                push_back_type(uint64_t);
-                push_back_type(int64_t);
-                push_back_type(uint32_t);
-                push_back_type(int32_t);
-                push_back_type(uint16_t);
-                push_back_type(int16_t);
-                push_back_type(uint8_t);
-                push_back_type(int8_t);
-                push_back_type(float);
-                push_back_type(double);
-                push_back_type(string);
 
-                //<< YAML::BeginMap;
-                //answer << YAML::Key << value << YAML::Value << "";
-                //answer << YAML::EndMap;
-                
-//                string ln_dt = service_datatype_to_ln(key);
-//                //            int ln_dt_size = ln_datatype_size(ln_dt);
-//
-//                if (ln_dt == "char*") {
-//                    const string& tmp_string = service_response[i++];
-//                    ((uint32_t *)adr)[0] = (uint32_t)tmp_string.size();
-//                    adr += 4;
-//                    if (tmp_string.size()) {
-//                        ((const char **)adr)[0] = (const char *)strdup(tmp_string.c_str());
-//                        to_delete.push_back((uint8_t *)(((const char **)adr)[0]));
-//                    } else 
-//                        ((const char **)adr)[0] = NULL;
-//                    adr += sizeof(char *);
-//                } else if (starts_with(key, "vector")) {
-//                    const size_t equals_idx = key.find_first_of('/');
-//                    if (std::string::npos != equals_idx)
-//                    {
-//                        //signature "uint32_t 4 1,[uint32_t 4 1,char* 1 1]* 8 1|uint32_t 4 1,[uint32_t 4 1,char* 1 1]* 8 1"
-//
-//                        string vector = key.substr(0, equals_idx);
-//                        string real_key = key.substr(equals_idx + 1);
-//                        string ln_dt = service_datatype_to_ln(real_key);
-//
-//                        const std::vector<robotkernel::rk_type> elem = service_response[i++];
-//
-//#define add_vector_type(type) \
-//                        if (ln_dt == #type) {                                                                       \
-//                            ((uint32_t *)adr)[0] = (uint32_t)elem.size();                                           \
-//                            adr += 4;                                                                               \
-//                            \
-//                            type* entries = new type[elem.size()];                                                  \
-//                            to_delete.push_back((uint8_t *)entries);                                                \
-//                            \
-//                            for (unsigned i = 0; i < elem.size(); ++i) {                                            \
-//                                entries[i] = (type)elem[i];                                                         \
-//                            }                                                                                       \
-//                            ((type **)adr)[0] = entries;                                                            \
-//                            adr += sizeof(type *);                                                                  \
-//                        }
-//
-//#define add_vector_type_char(type) \
-//                        if (ln_dt == #type) {                                                                       \
-//                            ((uint32_t *)adr)[0] = (uint32_t)elem.size();                                           \
-//                            adr += 4;                                                                               \
-//                            \
-//                            ln_vector_t* entries = new ln_vector_t[elem.size()];                                    \
-//                            to_delete.push_back((uint8_t *)entries);                                                \
-//                            \
-//                            for (unsigned i = 0; i < elem.size(); ++i) {                                            \
-//                                string entry = elem[i];                                                             \
-//                                entries[i].len = entry.length();                                                    \
-//                                entries[i].val = (const uint8_t *)(strdup(entry.c_str()));                          \
-//                                to_delete.push_back((uint8_t *)entries[i].val);                                     \
-//                            }                                                                                       \
-//                            ((ln_vector_t **)adr)[0] = entries;                                                     \
-//                            adr += sizeof(void*);                                                                   \
-//                        }
-//
-//                        add_vector_type(uint64_t);
-//                        add_vector_type(int64_t);
-//                        add_vector_type(uint32_t);
-//                        add_vector_type(int32_t);
-//                        add_vector_type(uint16_t);
-//                        add_vector_type(int16_t);
-//                        add_vector_type(uint8_t);
-//                        add_vector_type(int8_t);
-//                        add_vector_type(float);
-//                        add_vector_type(double);
-//                        add_vector_type_char(char*);
-//                    }
-//                } else if (ends_with(ln_dt, string("*"))) {
-//                    ((uint32_t *)adr)[0] = service_response[i++];
-//                    adr += 4;
-//
-//#define push_back_type(type) \
-//                    if (ln_dt == #type) {                                               \
-//                        ((type*)adr)[0] = service_response[i++]; \
-//                        adr += sizeof(type);                                            \
-//                    }
-//
-//                    push_back_type(uint64_t*);
-//                    push_back_type(int64_t*);
-//                    push_back_type(uint32_t*);
-//                    push_back_type(int32_t*);
-//                    push_back_type(uint16_t*);
-//                    push_back_type(int16_t*);
-//                    push_back_type(uint8_t*);
-//                    push_back_type(int8_t*);
-//                    push_back_type(float*);
-//                    push_back_type(double*);
-//                } else {
-//                    push_back_type(uint64_t);
-//                    push_back_type(int64_t);
-//                    push_back_type(uint32_t);
-//                    push_back_type(int32_t);
-//                    push_back_type(uint16_t);
-//                    push_back_type(int16_t);
-//                    push_back_type(uint8_t);
-//                    push_back_type(int8_t);
-//                    push_back_type(float);
-//                    push_back_type(double);
-//                }
+                    push_back_type(uint64_t);
+                    push_back_type(int64_t);
+                    push_back_type(uint32_t);
+                    push_back_type(int32_t);
+                    push_back_type(uint16_t);
+                    push_back_type(int16_t);
+                    push_back_type(uint8_t);
+                    push_back_type(int8_t);
+                    push_back_type(float);
+                    push_back_type(double);
+                    push_back_type(string);
+#undef push_back_type
+                }
             }
         }
     }
