baf763af6904ea48e9e166d13224415482cfa7ca
Robert Burger <robert.burger@dlr.de>
2025-05-08 07:00:01 +0200

    fix: conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -7,5 +7,6 @@ class MainProject(ConanFile):
     name = "bridge_rest"
     description = "robotkernel-5 service bridge via rest api"
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    requires = "robotkernel/[~=5]@robotkernel/stable", "libhttpserver/0.18.2@3rdparty/unstable"
+    requires = "robotkernel/5.0.50@robotkernel/snapshot", "libhttpserver/0.18.2@3rdparty/unstable"
+    #requires = "robotkernel/[~=5]@robotkernel/stable", "libhttpserver/0.18.2@3rdparty/unstable"
 
