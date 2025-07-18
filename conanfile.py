404a64febf519b355f3346ef8ace043a0361c2fc
Robert Burger <robert.burger@dlr.de>
2025-07-18 11:13:45 +0200

    cleanup: project structure

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,9 @@ class MainProject(ConanFile):
     description = "robotkernel service bridge via rest api"
     exports_sources = ["*", "!.gitignore"]
 
+    def source(self):
+        self.run(f"sed 's/AC_INIT(.*/AC_INIT([bridge_rest], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+
     def requirements(self):
         self.requires("robotkernel/[~6]@robotkernel/unstable")
         self.requires("libhttpserver/0.18.2@3rdparty/stable")
